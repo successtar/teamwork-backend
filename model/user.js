ebffb76ccd4b8b25882bff9e3d98c616abca4cd2
Hammed <hammed@hollatags.com>
2019-11-28 15:34:39 +0100

    Update User

--- model/user.js
+++ model/user.js
@@ -222,74 +222,8 @@ const createUser = (req, res) => {
 
 
 
-
-const updateUser = (req, res) => {
-  const id = parseInt(req.params.id)
-  const { name, email } = req.body
-
-  db.query('UPDATE users SET name = $1, email = $2 WHERE id = $3', [name, email, id], (error, results) => {
-      if (error) {
-        throw error
-      }
-      res.status(200).send(`User modified with ID: ${id}`)
-    }
-  )
-}
-
-const getUsers = (req, res) => {
-
-  db.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {    
-
-    if (error) {
-
-      res.status(500).json({ 
-    								status: "error",
-									
-									data : error
-
-								});
-    }
-    else{
-
-    	res.status(200).json( {
-    								status: "success",
-									
-									data : results.rows
-
-								});
-    }
-  })
-}
-
-const getUserById = (req, res) => {
-
-  const id = parseInt(req.params.id)
-
-  db.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    res.status(200).json(results.rows)
-  })
-}
-
-
-const deleteUser = (req, res) => {
-  const id = parseInt(req.params.id)
-
-  db.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    res.status(200).send(`User deleted with ID: ${id}`)
-  })
-}
-
 module.exports = {
                     signIn,
-                    getUsers,
-                    getUserById,
-                    createUser,
-                    updateUser,
-                    deleteUser,
+                    
+                    createUser
                 };
